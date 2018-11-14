81dbf37b5a4fbd6af3f44df0cd81774998eec770
Hampus Hernstig <hampus.hartmanhernstig@elev.ga.lbs.se>
2018-11-14 14:20:23 +0100

    xd

---
+++ Assets/CameraLook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public bool invert;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        transform.Rotate(Vector3.right, invert?
+        Input.GetAxis("Mouse Y") :
+        -Input.GetAxis("Mouse Y"), Space.Self);
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+   public float moveSpeed;
+
+    // Use this for initialization
+    void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        controller.SimpleMove(transform.forward * moveSpeed * Input.GetAxis("Vertical"));
+        controller.SimpleMove(transform.right * moveSpeed * Input.GetAxis("Horizontal"));
+
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"), Space.World);
+        transform.Rotate(Vector3.right, Input.GetAxis("Mouse Y"), Space.Self);
+    }
+}
